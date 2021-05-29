cf17a0caf9ec025c828ffff39a6988d92388a47a
Rocío CM <81853717+Roagp@users.noreply.github.com>
2021-05-29 20:06:19 +0200

    Ejercicio Completado

---
+++ routes/posts.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var router = express.Router();
+
+const Post = require("../models/Post");
+
+router.get("/all", async (req, res) => {
+  const posts = await Post.findAll();
+  res.send(posts);
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const foundUser = await Post.findByPk(+id);
+  res.send(foundUser);
+});
+
+router.patch("/content?", async (req, res) => {
+  const { id } = req.params;
+
+  const { content } = req.body;
+
+  const affectedPost = await User.update({ content }, { where: { id } });
+  affectedRows
+    ? res.send(`THe number of rows affected is ${affectedPost}`)
+    : res.send("None row was affected");
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const editPost = await Post.update(req.body, { where: { id } });
+  editPost
+    ? res.send(`The row affected is ${editPost}`)
+    : res.send("Now rows affected");
+});
+
+router.post("/insert", async (req, res) => {
+  const createdPost = await Post.create(req.body);
+  res.send(createdPost.toJSON());
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const destroyPost = await Post.destroy({ where: { id } });
+
+  destroyPost
+    ? res.send(`The row deleted is ${destroyPost}`)
+    : res.send("Now rows deleted");
+});
+
+module.exports = router;
